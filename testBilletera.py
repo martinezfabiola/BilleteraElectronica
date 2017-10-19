2289b65a3594856e30eada7fbd1c60836d1c83dd
yariluciani <yari.luciani@yahoo.com>
2017-10-19 15:29:48 -0400

    Casos de prueba finales
    Agregamos dos casos de malicia mas y el caso de esquina, anteriormente estaba el de frontera, interior y otros de malicia

--- testBilletera.py
+++ testBilletera.py
@@ -47,3 +47,33 @@ class TestSuite(unittest.TestCase):
 		billeteraMari = BilleteraElectronica(01, "Mariano", "Jose", "Rodriguez", "Perez", 27888909, 0004, credito4, debito4)
 		recargaMari = billeteraMari.recargar(5000, 14/01/2017, 1234)
 		consumoMari = billeteraMari.consumir(3000, 15/01/2017, 1234, 0005)
+
+	
+	# Caso Esquina: consume mas de lo que tiene en el saldo y el pin de consumo es distinto al que tiene registrado
+	def testEsquina(self):
+		print("Esquina")
+		credito5 = Creditos(1000, 26/11/2011, 0000) 
+		debito5 = Debitos(0, 27/11/2011, 1234)
+		billeteraPablo = BilleteraElectronica(01, "Pablo", "Eduardo", "Gonzalez", "Castro", 26455689, 0006, credito5, debito5)
+		recargaPablo = billeteraPablo.recargar(1000, 14/01/2017, 1234)
+		consumoPablo = billeteraPablo.consumir(1001, 15/01/2017, 1234, 0007)
+
+	# Caso Malicia: consumo sea un valor negativo 
+	def testMalicia3(self):
+		print("Malicia 3")
+		credito6 = Creditos(1000, 26/11/2011, 0000) 
+		debito6 = Debitos(0, 27/11/2011, 1234)
+		billeteraPedro = BilleteraElectronica(01, "Pedro", "Enrique", "Guitierrez", "Cedeño", 26455689, 0010, credito6, debito6)
+		recargaPedro = billeteraPedro.recargar(1000, 14/01/2017, 1234)
+		consumoPedro = billeteraPedro.consumir(-1001, 15/01/2017, 1234, 0010)
+
+	# Caso Malicia: recarga sea un valor negativo 
+	def testMalicia4(self):
+		print("Malicia 4")
+		credito7 = Creditos(1000, 26/11/2011, 0000) 
+		debito7 = Debitos(0, 27/11/2011, 1234)
+		billeteraCarlos = BilleteraElectronica(01, "Carlos", "Miguel", "Hernandez", "Suarez", 26450089, 0011, credito7, debito7)
+		recargaCarlos = billeteraCarlos.recargar(-1000, 14/01/2017, 1234)
+
+if __name__ == '__main__':
+	unittest.main()
