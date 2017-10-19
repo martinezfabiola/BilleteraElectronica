8a34f5f54052594b2767b44ba8388586a541d659
martinezfabiola <fabimartinezperez@gmail.com>
2017-10-19 15:20:03 -0400

    Caso de Prueba Interior

--- testBilletera.py
+++ testBilletera.py
@@ -20,3 +20,12 @@ class TestSuite(unittest.TestCase):
 		billeteraFabi = BilleteraElectronica(01, "Fabi", "Mercedes", "Martinez", "Perez", 26027714, 0001, credito, debito)
 		recargaFabi = billeteraFabi.recargar(3580, 14/01/2017, 1234)
 		consumoFabi = billeteraFabi.consumir(3580, 15/01/2017, 1234, 0001)
+
+	# Caso Interior: se mconsume 999 de los 1000
+	def testInterior(self):
+		print("Interior")
+		credito2 = Creditos(1000, 26/11/2011, 0000) 
+		debito2 = Debitos(0, 27/11/2011, 1234)
+		billeteraYari = BilleteraElectronica(01, "Yari", "Gabriela", "Luciani", "Faccini", 26127714, 0002, credito2, debito2)
+		recargaYari = billeteraYari.recargar(1000, 14/01/2017, 1234)
+		consumoYari = billeteraYari.consumir(999, 15/01/2017, 1234, 0002)
